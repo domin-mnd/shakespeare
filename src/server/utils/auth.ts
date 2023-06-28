2c6bc0bf519465b8d98f7aaaf80b9e1b5a6f2c80
Domin-MND <dominus.the.creator@gmail.com>
2023-06-28 22:03:10 +0300

    fix: remove polyfills

--- src/server/utils/auth.ts
+++ src/server/utils/auth.ts
@@ -3,9 +3,6 @@ import { h3 } from "lucia-auth/middleware";
 import prisma from "@lucia-auth/adapter-prisma";
 import { prisma as prismaClient } from "@/server/libs/database";
 
-// Polyfill crypto global
-import "lucia-auth/polyfill/node";
-
 export const auth = lucia({
   adapter: prisma(prismaClient),
   env: process.env.NODE_ENV === "development" ? "DEV" : "PROD",
